06d8c6aea6ca1b523d7709aea50ac687d642f7ae
Florian Lenz <flenz81@googlemail.com>
2017-10-20 16:15:26 +0200

    [fs] changed api

--- FileSystemInterface.ts
+++ FileSystemInterface.ts
@@ -4,22 +4,29 @@ export default interface FileSystemInterface {
      *
      * @param {string} fileName
      * @param {string} content
-     * @returns {Promise<string>}
+     * @returns {Promise<void>}
      */
-    writeFile(fileName: string, content: string): Promise<{}>
+    writeFile(fileName: string, content: string): Promise<void>
 
     /**
      *
      * @param {string} fileName
-     * @returns {boolean}
+     * @returns {Promise<boolean>}
      */
-    fileExist(fileName: string): Promise<{}>;
+    fileExist(fileName: string): Promise<boolean>;
 
     /**
      *
      * @param {string} fileName
-     * @returns {Promise<string>}
+     * @returns {Promise<any>}
      */
-    readFile(fileName:string) : Promise<{}>;
+    readFile(fileName:string) : Promise<any>;
+
+    /**
+     *
+     * @param {string} fileName
+     * @returns {Promise<void>}
+     */
+    deleteFile(fileName:string) : Promise<void>;
 
 }
